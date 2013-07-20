fceddb8e8dfc67c2872ba8aad41ba6eb1ce1d897
yzprofile <yzprofiles@gmail.com>
2013-07-21 01:42:20 +0800

    Added sync_msg.h

---
+++ ngx_sync_msg.h
@@ -0,0 +1,46 @@
+#ifndef _NGX_SYNC_MSG_H_INCLUDE_
+#define _NGX_SYNC_MSG_H_INCLUDE_
+
+
+#include <ngx_core.h>
+#include <ngx_event.h>
+#include <ngx_http.h>
+
+
+typedef struct {
+    ngx_str_t                            shm_name;
+    ngx_uint_t                           shm_size;
+    ngx_msec_t                           read_msg_timeout;
+} ngx_sync_msg_main_conf_t;
+
+
+typedef struct ngx_sync_msg_status_s {
+    ngx_pid_t                            pid;
+    ngx_msec_t                           time;
+} ngx_sync_msg_status_t;
+
+
+typedef struct ngx_sync_msg_shctx_s {
+    ngx_queue_t                          msg_queue;
+    ngx_uint_t                           version;
+    ngx_sync_msg_status_t               *status;
+} ngx_sync_msg_shctx_t;
+
+
+typedef struct ngx_sync_msg_global_ctx_s {
+    ngx_event_t                          msg_timer;
+    ngx_slab_pool_t                     *shpool;
+    ngx_sync_msg_shctx_t                *sh;
+} ngx_sync_msg_global_ctx_t;
+
+
+typedef struct ngx_sync_msg_s {
+    ngx_queue_t                          queue;
+    ngx_str_t                            title;
+    ngx_str_t                            content;
+    ngx_int_t                            count;
+    ngx_pid_t                           *pid;
+} ngx_sync_msg_t;
+
+
+#endif
--- ngx_sync_msg_module.c
+++ ngx_sync_msg_module.c
@@ -1,47 +1,9 @@
-#include <ngx_core.h>
-#include <ngx_event.h>
-#include <ngx_http.h>
+#include <ngx_sync_msg.h>
 
 
 #define NGX_SYNC_MSG_SHM_NAME_LEN 256
 #define ngx_sync_msg_add_timer(ev, timeout)                     \
     if (!ngx_exiting && !ngx_quit) ngx_add_timer(ev, (timeout))
-
-
-typedef struct {
-    ngx_str_t                            shm_name;
-    ngx_uint_t                           shm_size;
-    ngx_msec_t                           read_msg_timeout;
-} ngx_sync_msg_main_conf_t;
-
-
-typedef struct ngx_sync_msg_status_s {
-    ngx_pid_t                            pid;
-    ngx_msec_t                           time;
-} ngx_sync_msg_status_t;
-
-
-typedef struct ngx_sync_msg_shctx_s {
-    ngx_queue_t                          msg_queue;
-    ngx_uint_t                           version;
-    ngx_sync_msg_status_t               *status;
-} ngx_sync_msg_shctx_t;
-
-
-typedef struct ngx_sync_msg_global_ctx_s {
-    ngx_event_t                          msg_timer;
-    ngx_slab_pool_t                     *shpool;
-    ngx_sync_msg_shctx_t                *sh;
-} ngx_sync_msg_global_ctx_t;
-
-
-typedef struct ngx_sync_msg_s {
-    ngx_queue_t                          queue;
-    ngx_str_t                            title;
-    ngx_str_t                            content;
-    ngx_int_t                            count;
-    ngx_pid_t                           *pid;
-} ngx_sync_msg_t;
 
 
 static void *ngx_sync_msg_create_main_conf(ngx_conf_t *cf);
@@ -379,8 +341,6 @@ ngx_sync_msg_read_msg_locked(ngx_event_t *ev)
 
     sh = ngx_sync_msg_global_ctx.sh;
     shpool = ngx_sync_msg_global_ctx.shpool;
-
-    sh->version++;
 
     tp = ngx_timeofday();
     now = (ngx_msec_t) (tp->sec * 1000 + tp->msec);
